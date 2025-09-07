ceeb4d6f9ecaff17eebfbea646dfb32fb92454fa
mhaegens <michiel.haegens@gmail.com>
2025-09-07 18:45:57 +0200

    Use rotations to command pick workflow

--- processes/pick_assembly_quality.py
+++ processes/pick_assembly_quality.py
@@ -1,20 +1,32 @@
-"""Pick from assembly side and place on quality side."""
+"""Pick from assembly side and place on quality side using absolute poses."""
 
 from typing import Any
 
 
 def run(arm) -> Any:
     """Execute the pick-assembly-quality workflow."""
-    arm.goto_pose("home", speed=60)
-    arm.move("relative", {"A": -90}, speed=50)
-    arm.move("relative", {"D": 30}, speed=30)  # open claw
-    arm.move("relative", {"B": -20}, speed=40)  # move down
-    arm.move("relative", {"D": -30}, speed=30)  # close claw
-    arm.move("relative", {"B": 20}, speed=40)  # move up
-    arm.move("relative", {"A": 180}, speed=50)  # rotate to quality side
-    arm.move("relative", {"B": -20}, speed=40)  # move down
-    arm.move("relative", {"D": 30}, speed=30)  # open claw
-    arm.move("relative", {"B": 20}, speed=40)  # move up
-    arm.move("relative", {"D": -30}, speed=30)  # close claw
-    return arm.goto_pose("home", speed=60)
+    speed = 100  # top speed
+
+    # Backlash is measured in degrees so 5 rotations == 5 * 360
+    arm.set_backlash({"D": 5 * 360})
+
+    # The provided pose values are in raw motor degrees and are all multiples of
+    # 360.  Large degree values appear to be ignored by the Build HAT firmware,
+    # resulting in tiny movements.  Converting to rotations and using the
+    # ``units="rotations"`` mode keeps the commands within the supported range
+    # and produces the expected motion.
+    steps = [
+        {"A": 0, "B": 0, "C": 0, "D": 0},
+        {"A": -20160 / 360, "B": 0, "C": -37800 / 360, "D": 43920 / 360},
+        {"A": -21600 / 360, "B": 0, "C": -34920 / 360, "D": 43920 / 360},
+        {"A": -21600 / 360, "B": 14400 / 360, "C": -9720 / 360, "D": 3600 / 360},
+        {"A": -21600 / 360, "B": 3240 / 360, "C": -29880 / 360, "D": -39600 / 360},
+        {"A": -20880 / 360, "B": 3240 / 360, "C": 2520 / 360, "D": -39600 / 360},
+        {"A": 0, "B": 0, "C": 0, "D": 0},
+    ]
+
+    result = None
+    for pose in steps:
+        result = arm.move("absolute", pose, speed=speed, units="rotations")
+    return result
 
