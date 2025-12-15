d4fe8da8b52b4fed2c28025e6bd632b34cf9e745
mhaegens <michiel.haegens@gmail.com>
2025-12-16 00:54:15 +0100

    Add Lego Arm Control V2 POD plugin

---
+++ pod-plugin-v2/Component.js
@@ -0,0 +1,39 @@
+sap.ui.define([
+  "sap/dm/dme/podfoundation/component/production/ProductionUIComponent",
+  "sap/ui/model/json/JSONModel"
+], function (ProductionUIComponent, JSONModel) {
+  "use strict";
+
+  return ProductionUIComponent.extend("lego.haegens.plugins.legoarmv2.Component", {
+    metadata: { manifest: "json" },
+
+    init: function () {
+      ProductionUIComponent.prototype.init.apply(this, arguments);
+
+      // Read properties coming from POD Designer
+      var compData = (this.getComponentData && this.getComponentData()) || {};
+      var props    = compData.properties || compData.config || {};
+
+      // Expose properties on a named model (optional, handy for bindings)
+      this.setModel(new JSONModel({ properties: props }), "pod");
+
+      // Bridge to window for simple consumption by controllers or plain JS
+      // Merge with any pre-existing bridged config (e.g., posted via postMessage)
+      try {
+        var bridge = (window.__POD_CONFIG__) || {};
+        window.__POD_CONFIG__ = Object.assign({}, bridge, props);
+        document.dispatchEvent(new CustomEvent("POD_CONFIG_UPDATED", {
+          detail: window.__POD_CONFIG__
+        }));
+      } catch (e) {
+        // non-fatal
+      }
+    },
+
+    /** Convenience accessor if you prefer calling from controllers */
+    getPodProperties: function () {
+      var m = this.getModel("pod");
+      return m ? (m.getProperty("/properties") || {}) : {};
+    }
+  });
+});
---
+++ pod-plugin-v2/builder/PropertyEditor.js
@@ -0,0 +1,65 @@
+sap.ui.define([
+  "sap/ui/model/resource/ResourceModel",
+  "sap/dm/dme/podfoundation/control/PropertyEditor"
+], function (ResourceModel, PropertyEditor) {
+  "use strict";
+
+  return PropertyEditor.extend("lego.haegens.plugins.legoarmv2.builder.PropertyEditor", {
+
+    constructor: function (sId, mSettings) {
+      PropertyEditor.apply(this, arguments);
+
+      // i18n setup (kept as-is from your original)
+      this.setI18nKeyPrefix("customComponentListConfig.");
+      this.setResourceBundleName("lego.haegens.plugins.legoarmv2.i18n.builder");
+      this.setPluginResourceBundleName("lego.haegens.plugins.legoarmv2.i18n.i18n");
+    },
+
+    /**
+     * Build the Designer UI. Keep your original fields and add the API config.
+     */
+    addPropertyEditorContent: function (oPropertyFormContainer) {
+      var oData = this.getPropertyData();
+
+      // Original controls
+      this.addSwitch(oPropertyFormContainer, "backButtonVisible", oData);
+      this.addSwitch(oPropertyFormContainer, "closeButtonVisible", oData);
+      this.addInputField(oPropertyFormContainer, "title", oData);
+      this.addInputField(oPropertyFormContainer, "text", oData);
+
+      // New: External API configuration
+      // Base URL for the LEGO Arm API (e.g., http://legopi.local:8000)
+      this.addInputField(oPropertyFormContainer, "baseUrl", oData);
+
+      // Auth mode (string). Use: none | basic | bearer | x-api-key
+      this.addInputField(oPropertyFormContainer, "authMode", oData);
+
+      // Credentials (only the relevant ones will be used at runtime)
+      this.addInputField(oPropertyFormContainer, "username", oData);
+      this.addInputField(oPropertyFormContainer, "password", oData);      // keep as plain input; controller treats it as secret
+      this.addInputField(oPropertyFormContainer, "bearerToken", oData);
+      this.addInputField(oPropertyFormContainer, "apiKey", oData);
+    },
+
+    /**
+     * Defaults shown when the plugin is first added in the POD Designer.
+     */
+    getDefaultPropertyData: function () {
+      return {
+        // existing
+        "backButtonVisible": true,
+        "closeButtonVisible": true,
+        "title": "legoarmv2",
+        "text": "LEGO Arm Control V2",
+
+        // new
+        "baseUrl": "http://legopi.local:8000",
+        "authMode": "none",       // none | basic | bearer | x-api-key
+        "username": "",
+        "password": "",
+        "bearerToken": "",
+        "apiKey": ""
+      };
+    }
+  });
+});
